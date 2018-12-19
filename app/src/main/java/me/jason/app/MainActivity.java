5058213f23bba9714db1131dbe48f060a8189f94
jason <ljc@lizhiweike.com>
2018-12-19 19:26:20 +0800

    Update rename

--- app/src/main/java/me/jason/app/MainActivity.java
+++ app/src/main/java/me/jason/app/MainActivity.java
@@ -3,7 +3,6 @@ package me.jason.app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import me.jason.autopagelayout.R;
 
 public class MainActivity extends AppCompatActivity {
 
