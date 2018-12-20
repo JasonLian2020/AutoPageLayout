6fc0aa0c8286298136f74e42a4e2b3ed80eef6f8
jason <ljc@lizhiweike.com>
2018-12-20 15:23:55 +0800

    Update demo

--- app/src/main/java/me/jason/app/MainActivity.java
+++ app/src/main/java/me/jason/app/MainActivity.java
@@ -3,6 +3,9 @@ package me.jason.app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import me.jason.app.ui.FirstActivity;
+import me.jason.app.ui.SecondActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Base On Activity
+        findViewById(R.id.buttonActivity).setOnClickListener(view -> {
+            FirstActivity.start(this);
+        });
+        // Base On Fragment
+        findViewById(R.id.buttonFragment).setOnClickListener(view -> {
+            SecondActivity.start(this);
+        });
+        // Base On View
+        findViewById(R.id.buttonView).setOnClickListener(view -> {
+            SecondActivity.start(this);
+        });
     }
 }
---
+++ app/src/main/java/me/jason/app/ui/SecondActivity.java
@@ -0,0 +1,28 @@
+package me.jason.app.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import me.jason.app.R;
+
+/**
+ * Project Name:AutoPageLayout
+ * Package Name:me.jason.app.ui
+ * Created by jason on 2018/12/20 15:16 .
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    public static void start(Activity activity) {
+        Intent intent = new Intent(activity, SecondActivity.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
