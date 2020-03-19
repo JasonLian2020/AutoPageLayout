a4bebcda84697d30056ba2eaaab6d76d2c924863
jason <itlanguage@163.com>
2020-03-19 10:38:34 +0800

    Update library support AndroidX

--- Library-AutoPageLayout/src/main/java/me/jason/autopagelayout/AutoPageLayout.java
+++ Library-AutoPageLayout/src/main/java/me/jason/autopagelayout/AutoPageLayout.java
@@ -3,10 +3,11 @@ package me.jason.autopagelayout;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Looper;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.UiThread;
-import android.support.v4.app.Fragment;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
